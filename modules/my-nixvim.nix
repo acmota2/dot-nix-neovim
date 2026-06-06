d7370348e02cb07365d0a80f2f8257f81294229d
acmota2 <acmota2@gmail.com>
2026-06-07 01:12:08 +0200

    fix(nixvim): corrected config scoping

--- modules/my-nixvim.nix
+++ modules/my-nixvim.nix
@@ -2,14 +2,7 @@
 {
   imports = [ ../nixvim ];
 
-  options.programs.my-nixvim = {
-    enable = lib.mkEnableOption "my-nixvim";
-    settings = lib.mkOption {
-      type = lib.types.attrs;
-      default = { };
-      description = "Nixvim module configuration overrides";
-    };
-  };
+  options.programs.my-nixvim.enable = lib.mkEnableOption "my-nixvim";
 
   config = lib.mkIf config.programs.my-nixvim.enable {
     programs.neovim.enable = true;
--- nixvim/default.nix
+++ nixvim/default.nix
@@ -4,53 +4,51 @@
   pkgs,
   ...
 }@inputs:
-lib.mkIf config.programs.my-nixvim.enable (
-  let
-    mcp-hub-bin = pkgs.writeShellScriptBin "mcp-hub" ''
-      exec ${pkgs.nodejs_24}/bin/npx -y mcp-hub@latest "$@"
-    '';
-  in
-  {
-    options.programs.my-nixvim.settings = {
-      enable = lib.mkEnableOption "Enable my-nixvim settings";
-      llm-integration.enable = lib.mkEnableOption "Enable LLM integration";
+let
+  mcp-hub-bin = pkgs.writeShellScriptBin "mcp-hub" ''
+    exec ${pkgs.nodejs_24}/bin/npx -y mcp-hub@latest "$@"
+  '';
+in
+{
+  options.programs.my-nixvim.settings = {
+    enable = lib.mkEnableOption "Enable my-nixvim settings";
+    llm-integration.enable = lib.mkEnableOption "Enable LLM integration";
+  };
+
+  config = lib.mkIf config.programs.my-nixvim.enable {
+    globals.mapleader = ",";
+    keymaps = import ./remaps.nix;
+    opts = import ./options.nix;
+
+    diagnostic.settings = {
+      virtual_lines.current_line = true;
+      virtual_text = true;
     };
 
-    config = {
-      globals.mapleader = ",";
-      keymaps = import ./remaps.nix;
-      opts = import ./options.nix;
-
-      diagnostic.settings = {
-        virtual_lines.current_line = true;
-        virtual_text = true;
-      };
-
-      colorschemes.tokyonight = {
-        enable = true;
-        settings.style = "night";
-      };
-
-      extraConfigLua = import ./lua.nix inputs;
-
-      plugins = lib.mkIf config.programs.my-nixvim.settings.llm-integration.enable (
-        import ./plugins.nix inputs
-      );
-
-      extraPackages = with pkgs; [
-        isort
-        nixfmt
-        mcp-hub-bin
-        prettierd
-        shfmt
-        yaml-language-server
-      ];
-
-      extraPlugins = with pkgs; [
-        vimPlugins.mcphub-nvim
-      ];
-
-      lsp = import ./lsp.nix;
+    colorschemes.tokyonight = {
+      enable = true;
+      settings.style = "night";
     };
-  }
-)
+
+    extraConfigLua = import ./lua.nix inputs;
+
+    plugins = lib.mkIf config.programs.my-nixvim.settings.llm-integration.enable (
+      import ./plugins.nix inputs
+    );
+
+    extraPackages = with pkgs; [
+      isort
+      nixfmt
+      mcp-hub-bin
+      prettierd
+      shfmt
+      yaml-language-server
+    ];
+
+    extraPlugins = with pkgs; [
+      vimPlugins.mcphub-nvim
+    ];
+
+    lsp = import ./lsp.nix;
+  };
+}
